b4a8af67b9b74ef8b766df864475b70c7769e512
dc0059 <dc0059@uah.edu>
2017-02-02 15:23:05 -0600

    Missed in last commit

---
+++ CS499.TCMS/CS499.TCMS.Model/IModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace CS499.TCMS.Model
+{
+    /// <summary>
+    /// This interface will expose common model class functions and properies
+    /// </summary>
+    public interface IModel : IDataErrorInfo
+    {
+
+        /// <summary>
+        /// Flag indicating the model passes the verification test
+        /// </summary>
+        bool IsValid { get; }
+
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
+        long ID { get; set; }
+
+    }
+}
