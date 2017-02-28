405a253e6611a952013eeeb13730e003fd62c9a2
James <jtw0014@uah.edu>
2017-02-27 19:02:06 -0600

    Generic IRepository. Map function is NOT completed. Need a second look.

---
+++ CS499.TCMS/CS499.TCMS.DataAccess/IRepositoryInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CS499.TCMS.DataAccess
+{
+    /// <summary>
+    /// Exposes a generic interface that all interfaces will inherit from
+    /// </summary>
+    public interface IRepository
+    {
+        T getSingle<T>();
+        IEnumerator getAll();
+        bool Update();                      //return true on success
+        bool Delete<T>(T obj);              //return true on success
+        bool Insert<T>(T obj);              //return true on success
+
+
+        void Map();                         //NOT FINISHED. WILL HAVE AN IDATAREADER PARAMETER WHEN FINISHED.
+
+
+        
+    }
+}
