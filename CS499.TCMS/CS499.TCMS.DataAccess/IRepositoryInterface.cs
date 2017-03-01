27b38dc9bb94d43b7c052e4176b844636df0e990
James <jtw0014@uah.edu>
2017-02-28 19:09:04 -0600

    fixed the interface to final implementation. This is our base repository interface.

--- CS499.TCMS/CS499.TCMS.DataAccess/IRepositoryInterface.cs
+++ CS499.TCMS/CS499.TCMS.DataAccess/IRepositoryInterface.cs
@@ -10,18 +10,46 @@ namespace CS499.TCMS.DataAccess
     /// <summary>
     /// Exposes a generic interface that all interfaces will inherit from
     /// </summary>
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
-        T getSingle<T>();
-        IEnumerator getAll();
-        bool Update();                      //return true on success
-        bool Delete<T>(T obj);              //return true on success
-        bool Insert<T>(T obj);              //return true on success
+
+        /// <summary>
+        /// Generic method to get the specific object by its ID
+        /// </summary>
+        /// <param name="id">unique identifier</param>
+        /// <returns>the object based on the ID or null if its not found</returns>
+        T getSingle(object id);
 
 
-        void Map();                         //NOT FINISHED. WILL HAVE AN IDATAREADER PARAMETER WHEN FINISHED.
+        /// <summary>
+        /// Generic method to get all objects in the list
+        /// </summary>
+        /// <returns>a list of all objects</returns>
+        IEnumerator<T> getAll();
 
 
+        /// <summary>
+        /// Generic method to update the object
+        /// </summary>
+        /// <param name="model">the object to update</param>
+        void Update(T model);
+
+
+        /// <summary>
+        /// Generic method to delete the object
+        /// </summary>
+        /// <param name="model">the object to be deleted</param>
+        void Delete(T model);
+
+
+        /// <summary>
+        /// Generic method to insert a new object
+        /// </summary>
+        /// <param name="model">the object to be inserted</param>
+        void Insert(T model);
+
+
+   
         
     }
 }
