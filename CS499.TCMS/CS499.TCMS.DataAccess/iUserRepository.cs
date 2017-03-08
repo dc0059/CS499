e96558ff3bb2fad0155088c6c01ada0efb7fd5c2
Zachtj <vanthus01@gmail.com>
2017-03-08 17:06:57 -0600

    Setup the database connection and added data access unit tests

--- CS499.TCMS/CS499.TCMS.DataAccess/iUserRepository.cs
+++ CS499.TCMS/CS499.TCMS.DataAccess/iUserRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using ToolKit.Data;
 using CS499.TCMS.Model;
 
