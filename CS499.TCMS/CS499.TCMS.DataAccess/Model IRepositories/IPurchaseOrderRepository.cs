5c7b834bc1fa8af8296264a5a423ff6f7c1319c6
James <James@JAMES2014>
2017-03-11 14:42:39 -0600

    Added proper inheritance and using directives to Repository Interfaces

--- CS499.TCMS/CS499.TCMS.DataAccess/Model IRepositories/IPurchaseOrderRepository.cs
+++ CS499.TCMS/CS499.TCMS.DataAccess/Model IRepositories/IPurchaseOrderRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CS499.TCMS.Model;
 
 namespace CS499.TCMS.DataAccess.Model_IRepositories
 {
-    interface IPurchaseOrderRepository
+    interface IPurchaseOrderRepository : IRepository<PurchaseOrder>
     {
     }
 }
