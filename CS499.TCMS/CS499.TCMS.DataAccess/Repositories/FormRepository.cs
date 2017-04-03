e47f0e248143d923093cfdf6f2c15e65841c9c37
Zachtj <vanthus01@gmail.com>
2017-04-03 13:01:27 -0500

    Added classes for the form repository

---
+++ CS499.TCMS/CS499.TCMS.DataAccess/Repositories/FormRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolKit.Data;
+using CS499.TCMS.DataAccess.IRepositories;
+
+namespace CS499.TCMS.DataAccess.Repositories
+{
+    internal class FormRepository: IFormRepository
+    {
+    }
+}
