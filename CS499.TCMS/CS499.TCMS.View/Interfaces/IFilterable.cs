fe4f0a169647f6f43fe0810380d692aded14b8f9
dc0059 <dc0059@uah.edu>
2017-04-10 20:52:40 -0500

    na

---
+++ CS499.TCMS/CS499.TCMS.View/Interfaces/IFilterable.cs
@@ -0,0 +1,27 @@
+﻿using CS499.TCMS.View.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS499.TCMS.View.Interfaces
+{
+    /// <summary>
+    /// This interface will be used to apply filters to ViewModels
+    /// </summary>
+    public interface IFilterable
+    {
+
+        #region Methods
+
+        /// <summary>
+        /// Apply filter
+        /// </summary>
+        /// <param name="filter">the filter</param>
+        void ApplyFilter(Filter filter);
+
+        #endregion
+
+    }
+}
---
+++ CS499.TCMS/CS499.TCMS.View/Models/Filter.cs
@@ -0,0 +1,48 @@
+﻿using CS499.TCMS.View.Interfaces;
+
+namespace CS499.TCMS.View.Models
+{
+    /// <summary>
+    /// This class will contain filter information for a ViewModel
+    /// </summary>
+    public class Filter
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Filter"/> class.
+        /// </summary>
+        /// <param name="propertyName">Name of the property to filter</param>
+        /// <param name="filterText">Text to filter</param>
+        /// <param name="viewModel">ViewModel to apply the filter on</param>
+        public Filter(string propertyName, string filterText, IFilterable viewModel)
+        {
+            this.PropertyName = propertyName;
+            this.FilterText = filterText;
+            this.ViewModel = viewModel;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Name of the property to filter
+        /// </summary>
+        public string PropertyName { get; set; }
+
+        /// <summary>
+        /// Text to filter
+        /// </summary>
+        public string FilterText { get; set; }
+
+        /// <summary>
+        /// ViewModel to apply the filter on
+        /// </summary>
+        public IFilterable ViewModel { get; set; }
+
+        #endregion
+
+    }
+}
